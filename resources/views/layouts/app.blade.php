366dda3707a6a3f3ef4aafff08914ac940a2f351
samuelmwangi <samuelmwangi729@gmail.com>
2019-11-18 17:51:47 +0300

    initia Commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -161,6 +161,4 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/icheck.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/select2.min.js"></script>
-    @include('layouts.scripts')
-</body>
 </html>
